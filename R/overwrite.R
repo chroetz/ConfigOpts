c313612e8d7057ce1ef901f45eb2c0d04ad171eb
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-09-22 15:14:44 +0200

    overwrite with function

--- R/overwrite.R
+++ R/overwrite.R
@@ -3,7 +3,9 @@ overwriteOpts <- function(opts, lst) {
   opts <- asOpts(opts)
   lst <- as.list(lst)
   for (nm in names(lst)) {
-    if (isOpts(opts[[nm]])) {
+    if (is.function(lst[[nm]])) {
+      opts[[nm]] <- lst[[nm]](opts[[nm]])
+    } else if (isOpts(opts[[nm]])) {
       opts[[nm]] <- overwriteOpts(opts[[nm]], lst[[nm]])
     } else {
       opts[[nm]] <- lst[[nm]]
