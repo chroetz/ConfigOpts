080fc5e537c241905af7bdaca44ab19872597702
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-11-28 11:15:23 +0100

    expand with tags

--- R/expand.R
+++ R/expand.R
@@ -18,10 +18,11 @@ expansionList <- function(...) {
 expandList <- function(opts) {
   stopifnot(inheritsOptsClass(opts, "List"))
   listOfExpansionLists <- lapply(opts$list, .expandList)
-  opts$list <- unlist(listOfExpansionLists, recursive = FALSE)
+  opts$list <- unlist(listOfExpansionLists, recursive = FALSE, use.names = FALSE)
   return(opts)
 }
 
+# TODO: clean up code and create test
 .expandList <- function(x) {
   if (!is.list(x) || length(x) == 0) return(x)
   if (inherits(x, "expansion")) {
@@ -38,14 +39,61 @@ expandList <- function(opts) {
   needsExpansion <- sapply(x, \(y) isTRUE(attr(y, "expand")))
   x[!needsExpansion] <- lapply(x[!needsExpansion], .expandList)
   needsExpansion <- sapply(x, \(y) isTRUE(attr(y, "expand")))
-  tbl <- tidyr::expand_grid(!!!x[needsExpansion])
-  if (nrow(tbl) <= 1) return(x)
+  if (sum(needsExpansion) == 0) {
+    if (isTRUE(attr(x, "expand"))) return(x)
+    res <- list(x)
+    attr(res, "expand") <- TRUE
+    return(res)
+  }
+  expansionTags <- lapply(
+    x[needsExpansion],
+    \(a) {
+      nms <- names(a)
+      if (is.null(nms)) return(rep("*", length(a)))
+      return(nms)
+    })
+  tblValues <- tidyr::expand_grid(!!!x[needsExpansion])
+  tblTags <- tidyr::expand_grid(!!!expansionTags)
+  stopifnot(identical(dim(tblValues), dim(tblTags)))
+  tags <- validateTagsTable(tblTags)
+  tblValues <- tblValues[tags$valid,]
+  tags <- tags[tags$valid,]
   res <- list()
   attr(res, "expand") <- TRUE
-  for (i in seq_len(nrow(tbl))) {
+  for (i in seq_len(nrow(tblValues))) {
     entry <- x
-    for (nm in names(tbl)) entry[[nm]] <- tbl[[nm]][[i]]
+    for (nm in names(tblValues)) entry[[nm]] <- tblValues[[nm]][[i]]
     res[[i]] <- entry
+    names(res)[i] <- tags$name[i]
   }
   return(res)
 }
+
+validateTagsTable <- function(tblTags) {
+  res <- list(valid = logical(0), name = character(0))
+  for (i in seq_len(nrow(tblTags))) {
+    v <- validateTags(tblTags[i,])
+    res$valid[i] <- v$valid
+    res$name[i] <- v$name
+  }
+  return(as.data.frame(res))
+}
+
+validateTags <- function(tags) {
+  tags <- unlist(lapply(tags, strsplit, split=",", fixed=TRUE))
+  tags <- grep("[[:alpha:]]+[[:digit:]]+", tags, value = TRUE)
+  tagName <- sub("[[:digit:]]+", "", tags)
+  tagNr <- as.numeric(sub("[[:alpha:]]+", "", tags))
+  lst <- list()
+  uniNms <- unique(tagName)
+  for (j in seq_len(length(uniNms))) {
+    lst[[j]] <- unique(tagNr[tagName == uniNms[j]])
+  }
+  name = ""
+  valid <- all(sapply(lst, length) == 1)
+  if (valid) {
+    name <- paste0(uniNms, unlist(lst), collapse=",")
+  }
+  return(list(valid = valid, name = name))
+}
+
