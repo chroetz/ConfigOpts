fefa5ff5d07194f22724cf8ab4060ca6af342642
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-04-03 15:37:14 +0200

    writeOpts gains argument warn

--- R/files.R
+++ R/files.R
@@ -8,9 +8,10 @@
 #'   writing the object?
 #' @param digits max number of decimal digits to write for numeric values. Use
 #'   `I()` to specify significant digits. Use `NA` for max precision.
+#' @param warn Should warnings be signaled if, e.g., a file will be overwritten?
 #'
 #' @export
-writeOpts <- function(opts, file = NULL, dir = NULL, addMetaInfo = TRUE, digits = NA) {
+writeOpts <- function(opts, file = NULL, dir = NULL, addMetaInfo = TRUE, digits = NA, warn = TRUE) {
   if ((is.null(file) && is.null(dir)) || (!is.null(file) && !is.null(dir))) {
     stop("Exactly one of the arguments `dir` and `file` must be non-NULL.")
   }
@@ -27,7 +28,9 @@ writeOpts <- function(opts, file = NULL, dir = NULL, addMetaInfo = TRUE, digits 
     file <- paste0(file, ".json")
   }
   opts <- putClassAttributAsListEntry(opts)
-  if (file.exists(file)) warning(file, " will be overwritten")
+  if (warn) {
+    if (file.exists(file)) warning(file, " will be overwritten")
+  }
   jsonlite::write_json(opts, file, pretty = TRUE, digits = digits, auto_unbox = TRUE)
 }
 
